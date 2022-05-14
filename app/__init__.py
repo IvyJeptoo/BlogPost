7029fb8bb46fbcdd032a1be76a82508a7cd93c0e
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-14 18:06:13 +0300

    create database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DB_NAME = "blog.db"
 
 def  create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'savy'
+    app.config['SQALCHEMY_DATABASE_URI'] = F'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
+      
     from .main import main
     from .auth import auth
     
