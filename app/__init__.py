4e287faeff577bfe1832b7096a040476b7365038
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-15 02:19:38 +0300

    limit navbar details, add login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -10,19 +11,30 @@ def  create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'savy'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    # initializing the database
+    db.init_app(app)   
     
-      
+    
+    # importing blueprints 
     from .main import main
     from .auth import auth
     
-    
+    # registering blueprints
     app.register_blueprint(main)
     app.register_blueprint(auth)
-    
+    # importing models
     from .models import User
     
     create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     
     return app
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -2,22 +2,38 @@ from flask import flash, render_template,request,redirect, url_for
 from .import auth 
 from ..models import User
 from app import db
+from flask_login import login_user, login_required,logout_user,current_user
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template('register.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('logged in successfully',category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('main.home'))
+                
+            else:
+                flash('incorrect password,try again!', category='error')
+        else:
+            flash('email does not exist', category='error')
+    return render_template('register.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup',methods=['GET','POST'])
-def signup():
+def signup():    
     
     if request.method == 'POST':
         email = request.form.get('email')
@@ -25,21 +41,27 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        name = User.query.filter_by(username=username).first()
+        if user:
+            flash('email exists',category='error')        
+        elif name:
+            flash('username exists!',category='error')         
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters', category='error')
         elif len(username) < 2:
             flash('Username must be greater than 4 characters', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error') 
         elif len(password1) < 5:
-            flash('Password length sshould be more than 5', category='error')  
+            flash('Password length should be more than 5', category='error')  
             
         else:
             new_user = User(email=email,username=username,password=generate_password_hash(password1,method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created successfully', category='success')  
-            return redirect(url_for('main.home'))           
+            return redirect(url_for('auth.login'))           
             
     
-    return render_template('register.html')
+    return render_template('register.html',user=current_user)
