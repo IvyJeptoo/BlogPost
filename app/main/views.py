1919425c82ef677e8051c2c8536bc3124698f08a
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-15 15:40:56 +0300

    add user input to the database

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
 from . import main
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+from ..models import Blog
+from app import db
+
 
 @main.route('/')
 @login_required
@@ -13,7 +16,18 @@ def home():
 def quote():
     return render_template('quote.html')
 
-@main.route('/blog')
+@main.route('/blog',method=['GET','POST'])
 @login_required
 def blog():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        author = request.form.get('author')
+        content = request.form.get('content')        
+        user_id  = current_user._get_current_object().id
+        
+        new_blog = Blog(title=title,author=author, content=content, user_id=user_id)
+        db.session.add(new_blog)
+        db.session.commit()                   
+        
+    
     return render_template('blog.html')
